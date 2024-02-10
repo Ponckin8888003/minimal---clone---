8644560f2fea5942ba78fd103a7afff273a1a182
YUR4ST003 <98326379+Ponckin8888003@users.noreply.github.com>
2024-02-10 11:33:12 +0200

    Update form.js

--- js/form.js
+++ js/form.js
@@ -5,6 +5,7 @@ let emailInput = document.querySelector('#email');
 
 let waitingForm = document.querySelector('.waiting_form');
 let openFormButtons = document.querySelectorAll('.characteristics_button');
+let formBox = document.querySelector('.form_content');
 
 form.onsubmit = function(evt){
     evt.preventDefault();
@@ -16,7 +17,14 @@ form.onsubmit = function(evt){
 for (let openButton of openFormButtons) {
     openButton.onclick = function() {
         waitingForm.style.display = 'grid';
+        formBox.style.display = 'block';
+
+        document.addEventListener("click", function closeFormBox(e) {
+            if (!formBox.contains(e.target) && e.target !== openButton) {
+                formBox.style.display = 'none';
+                waitingForm.style.display = 'none'; 
+                document.removeEventListener("click", closeFormBox); 
+            }
+        });
     }
 }
-
-
